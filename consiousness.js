f259d3d6342066c03d5183d09ce0e9efc820ebe8
selvakumar <selvakumar.s@ippopay.com>
2023-04-30 13:21:52 +0530

    changes to created  consiouness page and server folder

---
+++ consiousness.js
@@ -0,0 +1,5 @@
+```Quantum computing is a rapidly-emerging technology that harnesses the laws of quantum mechanics to solve problems too complex for classical computers.  Today, IBM Quantum makes real quantum hardware -- a tool scientists only began to imagine 
+three decades ago -- available to hundreds of thousands of developers. Our engineers deliver ever-more-powerful 
+superconducting quantum processors at regular intervals, alongside crucial advances in software and quantum-classical orchestration. 
+This work drives toward the quantum computing speed and capacity necessary to change the world.  These machines are very different from the classical 
+computers that have been around for more than half a century. Here's a primer on this transformative technology.```
